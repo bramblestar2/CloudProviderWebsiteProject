b6086d7d57ff3ba9ac4bafa9f596a492cfda19c7
Jay Feather2000 <thegals4321@gmail.com>
2025-11-07 19:44:33 -0600

    Worked on Contact page to improve it

--- js/contact.js
+++ js/contact.js
@@ -1,4 +1,4 @@
-const form = document.querySelector('form');
+const form = document.getElementById('contactForm');
 const unavailableMessage = document.getElementById('unavailable');
 const submitSound = document.getElementById('submit-sound');
 
